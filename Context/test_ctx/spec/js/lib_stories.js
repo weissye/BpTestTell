dd30ed177cd2440f09049d69865ef7c9e9b1025c
Gera Weiss <gera.weiss@gmail.com>
2026-08-13 15:52:40 +0300

    Some comments made in a meating with Layan

--- Context/test_ctx/spec/js/lib_stories.js
+++ Context/test_ctx/spec/js/lib_stories.js
@@ -237,7 +237,11 @@ ctx.bthread("verifyCannotCreateBookWithBadParameters", "Book.All", function (boo
   });
 });
 
+// Gera: This is not consistent with the above because it is specific while above we use a generic query.
+// For example, "UserBook.CannotCreateHold" does not explain why while "UserBook.BookHasLoan" does explain why.
+// I think we should use the more generic query for all of them.
 ctx.bthread("verifyCannotDeleteLoanedBook", "UserBook.BookHasLoan", function (userbook) {
+  // Gera: This means that we must try this for every loan. I am not sure that this is wise.
   block(matchDeleteLoanOrBookOrUser(userbook.userid, userbook.bookid), function () {
     tryToDeleteBookAndExpectError(userbook.bookid);
   });
